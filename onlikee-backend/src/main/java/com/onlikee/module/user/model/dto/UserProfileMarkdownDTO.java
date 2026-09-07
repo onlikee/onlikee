d22317258ce9afe981b78770cfa81370fe2e205f
UnderHear <2533643340@qq.com>
2026-09-08 00:30:58 +0800

    用户markdown资料不得超过1000个字符

--- onlikee-backend/src/main/java/com/onlikee/module/user/model/dto/UserProfileMarkdownDTO.java
+++ onlikee-backend/src/main/java/com/onlikee/module/user/model/dto/UserProfileMarkdownDTO.java
@@ -1,10 +1,12 @@
 package com.onlikee.module.user.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserProfileMarkdownDTO {
     @NotNull(message = "content不能为空")
+    @Size(max = 1000, message = "content不得超过1000个字符")
     private String content;
 }
